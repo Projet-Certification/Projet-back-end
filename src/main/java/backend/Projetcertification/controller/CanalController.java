edb43ecd2de9851d34ec8be0f87f1e81f3d2d951
Orkall <sebastien.petit59280@gmail.com>
2023-12-01 14:17:22 +0100

    création du Post et du Getall

--- src/main/java/backend/Projetcertification/controller/CanalController.java
+++ src/main/java/backend/Projetcertification/controller/CanalController.java
@@ -4,12 +4,11 @@ import backend.Projetcertification.entity.Canal;
 import backend.Projetcertification.service.CanalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("canals")
@@ -19,5 +18,19 @@ public class CanalController {
     @GetMapping
     public ResponseEntity<List<Canal>> findAllCanal(){
         return ResponseEntity.ok(canalService.getCanaux());
+    }
+    @PostMapping
+    public Canal addCanal(@RequestBody Canal newCanal) {
+        canalService.addCanal(newCanal);
+        return newCanal;
+    }
+    @GetMapping("{id}")
+    public ResponseEntity <?> findCanalById(@PathVariable Integer id) {
+        Optional<Canal> optional = canalService.getCanalById(id);
+        if (optional.isPresent()) {
+            Canal canal = optional.get();
+            return ResponseEntity.ok(canal);
+        } else
+            return ResponseEntity.notFound().build();
     }
 }
