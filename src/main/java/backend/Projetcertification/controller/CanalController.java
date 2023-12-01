ce8e5b6e02a40853f1b50667d15fd4c53c7df636
Orkall <sebastien.petit59280@gmail.com>
2023-12-01 11:38:45 +0100

    création du du controller et service  canal

--- src/main/java/backend/Projetcertification/controller/CanalController.java
+++ src/main/java/backend/Projetcertification/controller/CanalController.java
@@ -1,9 +1,23 @@
 package backend.Projetcertification.controller;
 
+import backend.Projetcertification.entity.Canal;
+import backend.Projetcertification.service.CanalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("canals")
 public class CanalController {
+    @Autowired
+    CanalService canalService;
+    @GetMapping
+    public ResponseEntity<List<Canal>> findAllCanal(){
+        return ResponseEntity.ok(canalService.getCanaux());
+    }
 }
--- src/main/java/backend/Projetcertification/service/CanalService.java
+++ src/main/java/backend/Projetcertification/service/CanalService.java
@@ -1,7 +1,30 @@
 package backend.Projetcertification.service;
 
+import backend.Projetcertification.entity.Canal;
+import backend.Projetcertification.repository.CanalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CanalService {
+    @Autowired
+    CanalRepository canalRepository;
+    public List<Canal> getCanaux(){
+        return canalRepository.findAll();
+    }
+    public Optional<Canal> getCanalById(Integer id){
+        return canalRepository.findById(id);
+    }
+    public void addCanal(Canal canal){
+        canalRepository.save(canal);
+    }
+    public void updateCanal(Canal canal) {
+        canalRepository.save(canal);
+    }
+    public void deleteCanal(Integer id){
+        canalRepository.deleteById(id);
+    }
 }
