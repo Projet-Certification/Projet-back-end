a19c1bc60ca002a79e0bb5e6fb4245becd588930
Orkall <sebastien.petit59280@gmail.com>
2023-12-04 10:19:20 +0100

    fin du add

--- src/main/java/backend/Projetcertification/controller/CanalController.java
+++ src/main/java/backend/Projetcertification/controller/CanalController.java
@@ -12,8 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 
 @RestController
 @RequestMapping("canals")
